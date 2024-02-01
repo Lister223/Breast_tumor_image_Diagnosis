99a7729b9d321928fe10a7d63fe2c10f49f5a7bc
Lister <tyhood17@gmail.com>
2024-02-01 22:38:52 +0800

    新增圖片顯示與返回

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import model
-from flask import Flask,request,jsonify,render_template , redirect,url_for
+from flask import Flask,request,jsonify,render_template , redirect,url_for,send_file, make_response
 from flask_cors import CORS
 from PIL import Image
 import io
+import base64
 
 
 app = Flask(__name__)
@@ -16,13 +17,18 @@ def index():
 def form():
     return render_template('form.html')
 
-
 @app.route('/predict',methods=['POST'])
 def postInput():
     file = request.files['image']
     image = Image.open(io.BytesIO(file.read()))
     result = model.predict(image)
-    return (f'The diagnosis result of tumor: {result}.')
+    # 將圖片轉換為 base64 編碼
+    buf = io.BytesIO()
+    image.save(buf, format='PNG')
+    image_base64 = base64.b64encode(buf.getvalue()).decode('utf-8')
+    # 回傳一個包含圖片和預測結果的 HTML 頁面
+    return render_template('result.html',
+                           image_base64=image_base64, result=result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
